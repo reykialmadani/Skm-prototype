f4b3359e03ffb4b50db2c756d50b0e4fc85db6b2
reykialmadani <150756706+reykialmadani@users.noreply.github.com>
2026-09-22 17:07:16 +0700

    feat: move role quick access to login page only and clean header

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@
    ========================================================================== */
 
 document.addEventListener('DOMContentLoaded', () => {
-  initDemoSwitcherBar();
   initSidebarActiveState();
   initLucideIcons();
   initModalListeners();
